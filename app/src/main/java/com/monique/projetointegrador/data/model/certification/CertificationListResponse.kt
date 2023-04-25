3231c48510ade9f24766ce239cc9393a6fae4139
Romavic dos Anjos <romavicdosanjoskc@gmail.com>
2023-04-25 03:40:31 +0100

    renamed class

--- app/src/main/java/com/monique/projetointegrador/data/model/certification/CertificationListResponse.kt
+++ app/src/main/java/com/monique/projetointegrador/data/model/certification/CertificationListResponse.kt
@@ -2,4 +2,4 @@ package com.monique.projetointegrador.data.model.certification
 
 import com.google.gson.annotations.SerializedName
 
-class CertificationListReponse(@SerializedName("results") val results: List<CertificationResponse>)
+class CertificationListResponse(@SerializedName("results") val results: List<CertificationResponse>)
